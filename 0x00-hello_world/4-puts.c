187f0054fa6a2d0f3878562ff08f30e0b917a4d4
mercyndanu <mercynm01@gmail.com>
2022-12-08 06:20:38 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+/*
+ * main - prints exactly "Programming is like building a multilingual puzzle" using puts
+ *
+ * Return: 0
+ */
+int main(void)
+	puts("\|"Programming is like building a multilingual puzzle");
+	return (0);
