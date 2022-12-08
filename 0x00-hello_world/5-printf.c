7b841be99a33ede5880274cc35defe78194f0064
mercyndanu <mercynm01@gmail.com>
2022-12-08 06:53:02 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print code using printf 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
