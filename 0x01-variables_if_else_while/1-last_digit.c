a8ba5397bc77a060290e2a99ca3add4284a245e7
mercyndanu <mercynm01@gmail.com>
2022-12-09 13:26:34 -0800

    changes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,13 @@
 #include <time.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
+
 int main(void)
 {
 	int n;
