fae9b7941b84535f6c484878fb22504e8555d0f6
mercyndanu <mercynm01@gmail.com>
2022-12-09 13:54:56 -0800

    3-print_alphabets.c file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+
+	for (n = 'A'; n <= 'Z'; n++)
+	{
+		putchar(n);
+	}
+		putchar('\n');
+
+		return (0);
+}
