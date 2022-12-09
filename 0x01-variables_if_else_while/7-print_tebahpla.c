37375c38944da67f5379702e7b95bfb2f294f219
mercyndanu <mercynm01@gmail.com>
2022-12-09 14:33:20 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = 'z'; n >= 'a'; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
