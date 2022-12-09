634a239f7889c1930a92fab28e19fe42dbb0fc6c
mercyndanu <mercynm01@gmail.com>
2022-12-09 13:41:29 -0800

    2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char n;
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
