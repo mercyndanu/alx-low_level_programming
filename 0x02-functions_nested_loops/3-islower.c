718540090d7ee54cf11842a20642d6e5df5907ec
mercyndanu <mercynm01@gmail.com>
2022-12-14 05:26:31 -0800

    5-sign.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
+ *_islower - checks for a lowercase character
  *@c: the character to be checked
  * Retuen: 1 for lowercase character or 0 for anything else
  */
-int _islower(int c);
+int _islower(int c)
 {
 
-	if (c > 'a' && c < 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,21 +7,21 @@
  * Return: 1 for positive number, -1 for negative number or 0 for zero
  */
 
-int print_sign(int n);
+int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(33);
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar(35);
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(38);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
