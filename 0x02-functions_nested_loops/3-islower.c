2a9884ba437fc5122d20806345e9afb6868189e9
mercyndanu <mercynm01@gmail.com>
2022-12-14 04:20:53 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 /**
  * _islower - checks for lowercase character
  *@c: the character to be checked
- * Retuen: 1 for lowercasw character or 0 for anything else
+ * Retuen: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
 {
