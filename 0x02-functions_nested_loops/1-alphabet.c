76e6b3f3ccc6de47781142ad8c28660e07ae1de0
mercyndanu <mercynm01@gmail.com>
2022-12-14 01:02:10 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,9 +4,15 @@
  *
  * Return: aways 0
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
+	char c = 'a';
 
+	while (c <= 'z');
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
 	return (0);
 }
