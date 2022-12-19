9a4f44a2e9d89c995a3306d029289153c510d01c
mercyndanu <mercynm01@gmail.com>
2022-12-19 07:38:32 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  print_array - prints n element of an array of integers
+ *  @a: first int to check
+ *  @n: second int to check
+ *  Return: 0 is success
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	printf("\n");
+}
