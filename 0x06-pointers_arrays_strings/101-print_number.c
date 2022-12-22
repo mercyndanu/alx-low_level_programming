863e193105c7f4de9886e0a07dd0c626ffe518f2
mercyndanu <mercynm01@gmail.com>
2022-12-22 06:17:20 -0800

    102-magic.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_number - Prints any integer with putchar
+ * @n: Number to prints
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int i;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	i = n;
+
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar(i % 10 + '0');
+}
