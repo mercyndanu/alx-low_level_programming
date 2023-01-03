196a268f53d21c21ad8d87ffa9553cea6d77a64b
mercyndanu <mercynm01@gmail.com>
2023-01-03 04:45:30 -0800

    1-memcpy.c, 2-strchr.c, 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: in the string
+ * @c: occurrence of the character
+ * Return: a pointer to the first occurrence of the character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
