c3579f73b0859387170290037dabebe7ba477991
mercyndanu <mercynm01@gmail.com>
2023-01-05 02:02:37 -0800

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
+ */
+void _puts_recursion(char *s)
+{
+	while (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
