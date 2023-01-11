3f8f684476e5d3e940ac3a5c97716dd8d9475efc
mercyndanu <mercynm01@gmail.com>
2023-01-11 07:11:42 -0800

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * free_grid - frees the memory allocate for the grid
+ * free_grid - frees the memory allocate for the
+ * grid created by alloc_grid()
  * @grid: grid to free
  * @height: height of the grid
  * Return: 0
