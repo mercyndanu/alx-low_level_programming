51d5b6d506f7af9a19e7acd03f46e84d4836588e
mercyndanu <mercynm01@gmail.com>
2023-04-10 01:53:07 -0700

    correction

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @n: the integer
+ * Return: the abs of the integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
